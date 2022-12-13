895a7b47d0c948b7bb97880838a32dbdb26d5902
Danny Abdi <zhac232@live.rhul.ac.uk>
2022-12-13 22:26:43 +0000

    Finished CalcController

--- calculator/src/main/java/calculator/CalcController.java
+++ calculator/src/main/java/calculator/CalcController.java
@@ -1,16 +1,47 @@
 package calculator;
 
+/**
+ * Controller class that handles interaction between interface and model.
+ * 
+ * @author danny
+ *
+ */
 public class CalcController {
 	private CalcModel model;
 	private ViewInterface view;
 	private boolean isInfix;
+	
+	/**
+	 * Adds methods as observers.
+	 * 
+	 * @param m instance of CalcModel.
+	 * @param v instance of ViewInterface.
+	 */
+	public CalcController(CalcModel m, ViewInterface v) {
+		this.model = m;
+		this.view = v;
+		view.addCalcObserver(this::calculate);
+		view.addTypeObserver(this::expressionType);
+	}
 
+	/**
+	 * Takes the user input and evaluates it.
+	 */
 	public void calculate() {
-		String userInput = view.getUserInput();
-		model.evaluate(userInput, isInfix);
+		String userInput = view.getExpression();
+		try {
+			model.evaluate(userInput, isInfix);
+		} catch (InvalidExpressionException e) {
+			e.printStackTrace();
+		} catch (BadTypeException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	/**
+	 * Gets the type of the expression.
+	 */
 	public void expressionType() {
-		
+		view.retrieveOpType().getOpType();
 	}
 }
